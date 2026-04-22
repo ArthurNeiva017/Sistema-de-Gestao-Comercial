045fd8a404e79736441feea75afe39758f6f4abf
ArthurNeiva017 <arthur.b.neiva@gmail.com>
2026-04-22 18:32:28 -0300

    Atributos de Produto e Cliente

--- src/main/java/br/ceub/ProjetoFinal/model/Cliente.java
+++ src/main/java/br/ceub/ProjetoFinal/model/Cliente.java
@@ -2,5 +2,62 @@ package br.ceub.ProjetoFinal.model;
 
 public class Cliente {
 	
+	private int id;
+	private String nome;
+	private String cpf;
+	private String email;
+	private int telefone;
+	private String endereco;	
+	
+	public Cliente(int id, String nome, String cpf, String email, int telefone, String endereco) {
+		this.id = id;
+		this.nome = nome;
+		this.cpf = cpf;
+		this.email = email;
+		this.telefone = telefone;
+		this.endereco = endereco;
+	}
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(int telefone) {
+		this.telefone = telefone;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	
 
 }
+	
+	
+
