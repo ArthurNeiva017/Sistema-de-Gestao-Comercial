a0ade9d8b534cb6217381bf0f18d7bcb6b9971ef
ArthurNeiva017 <arthur.b.neiva@gmail.com>
2026-05-25 15:38:01 -0300

    Alteração na Model

--- src/main/java/br/ceub/ProjetoFinal/model/ItemVenda.java
+++ src/main/java/br/ceub/ProjetoFinal/model/ItemVenda.java
@@ -1,5 +1,71 @@
 package br.ceub.ProjetoFinal.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "itens_venda")
 public class ItemVenda {
+    
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+    @Column(name="produtoId", nullable = false)
+    private Produto produtoId;
+
+    @Column(name="quantidade", nullable = false)
+    private Integer quantidade;
+
+    @Column(name="precoUnitario", nullable = false)
+    private double precoUnitario;
+
+    public ItemVenda() {
+        super();
+    }
+
+    public ItemVenda(int id, Produto produtoId, int quantidade, double precoUnitario) {
+        this.id = id;
+        this.produtoId = produtoId;
+        this.quantidade = quantidade;
+        this.precoUnitario = precoUnitario;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Produto getProdutoId() {
+        return produtoId;
+    }
+
+    public void setProdutoId(Produto produtoId) {
+        this.produtoId = produtoId;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public double getPrecoUnitario() {
+        return precoUnitario;
+    }
+
+    public void setPrecoUnitario(double precoUnitario) {
+        this.precoUnitario = precoUnitario;
+    }
+    
 
 }
--- src/main/java/br/ceub/ProjetoFinal/model/Venda.java
+++ src/main/java/br/ceub/ProjetoFinal/model/Venda.java
@@ -1,5 +1,84 @@
 package br.ceub.ProjetoFinal.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "vendas")
 public class Venda {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Column(name="data", nullable = false)
+	private LocalDate data;
+
+	@Column(name="valorTotal", nullable = false)
+	private double valorTotal;
+
+    @Column(name="clienteId", nullable = false)z
+    private Cliente clienteId;
+    @Column(name="usuarioId", nullable = false)
+    private Usuario usuarioId;
+    
+    public Venda() {
+        super();
+    }
+
+    public Venda(int id, LocalDate data, double valorTotal, Cliente clienteId, Usuario usuarioId) {
+        this.id = id;
+        this.data = data;
+        this.valorTotal = valorTotal;
+        this.clienteId = clienteId;
+        this.usuarioId = usuarioId;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public double getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(double valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    public Cliente getClienteId() {
+        return clienteId;
+    }
+
+    public void setClienteId(Cliente clienteId) {
+        this.clienteId = clienteId;
+    }
+
+    public Usuario getUsuarioId() {
+        return usuarioId;
+    }
+
+    public void setUsuarioId(Usuario usuarioId) {
+        this.usuarioId = usuarioId;
+    }
+    
+    
+    
 
 }
