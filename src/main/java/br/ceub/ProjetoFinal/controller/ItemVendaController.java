7c5d393a11da99eac8e6c6d110697687fec0c726
ArthurNeiva017 <arthur.b.neiva@gmail.com>
2026-05-26 11:56:39 -0300

    Alteração na Controller

--- src/main/java/br/ceub/ProjetoFinal/controller/ItemVendaController.java
+++ src/main/java/br/ceub/ProjetoFinal/controller/ItemVendaController.java
@@ -1,5 +1,61 @@
 package br.ceub.ProjetoFinal.controller;
 
+@RestController
+@RequestMapping("/api/itensvenda")
 public class ItemVendaController {
+
+    @Autowired
+    private ItemVendaService itemVendaService;
+
+    @PostMapping
+    public ResponseEntity<ItemVenda> createItemVenda(@RequestBody ItemVenda itemVenda) {
+        ItemVenda savedItemVenda = itemVendaService.save(itemVenda);
+        return ResponseEntity.ok(savedItemVenda);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ItemVenda>> getAllItemVenda() {
+        List<ItemVenda> ItemVendas = itemVendaService.findAll();
+        return ResponseEntity.ok(ItemVendas);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ItemVenda> getItemVendaById(@PathVariable Integer id) {
+        Optional<ItemVenda> ItemVenda = itemVendaService.findById(id);
+        return ItemVenda.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<List<ItemVenda>> searchItemVendas(@RequestParam String nome) {
+        List<ItemVenda> ItemVendas = itemVendaService.findByNome(nome);
+        return ResponseEntity.ok(ItemVendas);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ItemVenda> updateItemVenda(@PathVariable Integer id, @RequestBody ItemVenda itemVendaDetails) {
+        Optional<ItemVenda> optionalItemVenda = itemVendaService.findById(id);
+        if (optionalItemVenda.isPresent()) {
+            ItemVenda itemVenda = optionalItemVenda.get();
+            itemVenda.setVendaId(itemVendaDetails.getVendaId());
+            itemVenda.setProdutoId(itemVendaDetails.getProdutoId());
+            itemVenda.setQuantidade(itemVendaDetails.getQuantidade());
+            itemVenda.setPrecoUnitario(itemVendaDetails.getPrecoUnitario());
+            ItemVenda updatedItemVenda = itemVendaService.save(itemVenda);
+            return ResponseEntity.ok(updatedItemVenda);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteItemVenda(@PathVariable Integer id) {
+        Optional<ItemVenda> itemVenda = itemVendaService.findById(id);
+        if (itemVenda.isPresent()) {
+            itemVendaService.deleteById(id);
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 
 }
--- src/main/java/br/ceub/ProjetoFinal/controller/VendaController.java
+++ src/main/java/br/ceub/ProjetoFinal/controller/VendaController.java
@@ -1,5 +1,64 @@
 package br.ceub.ProjetoFinal.controller;
 
+
+@RestController
+@RequestMapping("/api/vendas")
 public class VendaController {
+
+@Autowired
+	private VendaService vendaService;
+
+	@PostMapping
+	public ResponseEntity<Venda> createVenda(@RequestBody Venda venda) {
+		Venda savedVenda = vendaService.save(venda);
+		return ResponseEntity.ok(savedVenda);
+	}
+
+	@GetMapping
+	public ResponseEntity<List<Venda>> getAllVenda() {
+		List<Venda> Vendas = vendaService.findAll();
+		return ResponseEntity.ok(Vendas);
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Venda> getVendaById(@PathVariable Integer id) {
+		Optional<Venda> Venda = vendaService.findById(id);
+		return Venda.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+	}
+	
+	@GetMapping("/search")
+    public ResponseEntity<List<Venda>> searchVendas(@RequestParam String nome) {
+        List<Venda> Vendas = vendaService.findByNome(nome);
+        return ResponseEntity.ok(Vendas);
+    }
+	
+	@PutMapping("/{id}")
+    public ResponseEntity<Venda> updateVenda(@PathVariable Integer id, @RequestBody Venda vendaDetails) {
+        Optional<Venda>optionalVenda = vendaService.findById(id);
+        if (optionalVenda.isPresent()) {
+            Venda venda = optionalVenda.get();
+            venda.setData(vendaDetails.getData());
+            venda.setValorTotal(vendaDetails.getValorTotal());
+            venda.setClienteId(vendaDetails.getClienteId());
+            venda.setUsuarioId(vendaDetails.getUsuarioId());
+            produto.setQuantidade(produtoDetails.getQuantidade());
+            Produto updatedProduto = produtoService.save(produto);
+            return ResponseEntity.ok(updatedProduto);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+	 @DeleteMapping("/{id}")
+	    public ResponseEntity<Void> deleteVenda(@PathVariable Integer id) {
+	        Optional<Venda> venda = vendaService.findById(id);
+	        if (venda.isPresent()) {
+	            vendaService.deleteById(id);
+	            return ResponseEntity.noContent().build();
+	        } else {
+	            return ResponseEntity.notFound().build();
+	        }
+	    }
+
 
 }
