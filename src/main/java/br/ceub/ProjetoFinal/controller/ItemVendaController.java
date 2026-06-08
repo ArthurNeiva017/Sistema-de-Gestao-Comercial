60f4c03e9a0fa24d239502d71637b60842e3277b
Arthur Barroso Neiva <arthur.neiva@dinfor.uniceub>
2026-06-08 09:20:14 -0300

    Correções 2

--- src/main/java/br/ceub/ProjetoFinal/controller/ItemVendaController.java
+++ src/main/java/br/ceub/ProjetoFinal/controller/ItemVendaController.java
@@ -1,5 +1,23 @@
 package br.ceub.ProjetoFinal.controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.ceub.ProjetoFinal.model.ItemVenda;
+import br.ceub.ProjetoFinal.service.ItemVendaService;
+
 @RestController
 @RequestMapping("/api/itensvenda")
 public class ItemVendaController {
